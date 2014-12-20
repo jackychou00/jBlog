f6a3458419ac3532139ef5ac9817cddcabe6b069
Jacky Chou <jackychou00@hotmail.com>
2014-12-20 15:40:01 +0800

    更新版位設定 取消註冊email認證

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,:confirmable, :lockable, :timeoutable and :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable#,:confirmable, :lockable, :timeoutable and :omniauthable
 end
